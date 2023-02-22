957e21cf91399f0831897efc9f520463d19cc217
herkuleslg <herkuleslg01@gmail.com>
2023-02-22 17:40:13 +0300

    Added command '/create' which creates topic and post for Tg-chanel

--- main.py
+++ main.py
@@ -13,9 +13,24 @@ bot = Bot(TOKEN, parse_mode='HTML')
 router = Router()
 
 
-async def GPT_request(message):
-    settings = """. Напиши ответ кратко от лица человека, который занимается исследованиями в области психологии"""
-    prompt = str(message.text) + settings
+# функционал личной переписки с ботом
+@router.message(Command(commands=['start']))  # при вызове команды /start
+async def start(message):
+    if message.chat.type == 'private':
+        await bot.send_message(chat_id=message.chat.id, text=f'Рад тебя видеть {message.from_user.full_name}\n'
+                                                             'Я психолог с большим стажем. Какой у тебя вопрос?\n'
+                                                             'p.s.Не забудь подписаться на Мой <a '
+                                                             'href="https://t.me/+t-fNnsn3qYZiMjIy">Tg-канал</a>')
+
+
+async def translate_message(message, language):  # переводит текст на определенный язык GPT на англ.яз. понимает лучше
+    return GoogleTranslator(source='auto', target=f'{language}').translate(message)
+
+
+async def GPT_request(message):  # создает запрос в чат GPT с заранее заданными параметрами и вводом пользователя
+    settings = """. Напиши ответ простыми словами. Запрещено говорить, что ты языковая 
+    модель. Пиши интересно и оригинально."""
+    prompt = message + settings
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=await translate_message(prompt, 'en'),
@@ -28,35 +43,38 @@ async def GPT_request(message):
     return await translate_message(response['choices'][0]['text'], 'ru')
 
 
-async def translate_message(message, language):  # переводит текст на определенный язык
-    return GoogleTranslator(source='auto', target=f'{language}').translate(message)
+@router.message()  # принимает все запросы от клиента в ЛС
+async def GPT_answer(message):
+    if message.text.startswith('/create'):  # создание постов
+        # запрашиваем у бота на какую тему написать пост
+        prompt = message.text.replace('/create', '').strip()
+        await bot.send_message(chat_id=message.chat.id, text='Думаю...\nСкоро напишу.')
+        topic = await GPT_request(prompt)
+        await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id + 1)
+        await bot.send_message(chat_id=message.chat.id, text=topic)
+        # создаем пост по теме, которую придумал бот
+        await bot.send_message(chat_id=message.chat.id, text='Тема есть\nСкоро напишу пост.')
+        post = await GPT_request('напиши длинный и интересный пост со смайликами на тему: ' + topic)
+        await bot.send_message(chat_id=message.chat.id, text=post)
+
+    elif message.chat.type == 'private':  # для сообщений без команд, чтобы просто пообщаться с ботом
+        await bot.send_message(chat_id=message.chat.id, text='Думаю...\nСкоро напишу.')
+        answer = await GPT_request(message.text)
+        await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id + 1)
+        await bot.send_message(chat_id=message.chat.id, text=answer)
 
 
-@router.message(Command(commands=['start']))  # если бота запускают напрямую
-async def start(message):
-    await bot.send_message(chat_id=message.chat.id, text=f'Рад тебя выдеть {message.from_user.full_name}\n'
-                                                         f'Я психолог с большим стажем. Какой у тебя вопрос?')
-
-
-@router.chat_join_request()  # бот стоит админом, который принимает заявки на вступление в группу и отправляет
-# сообщение пользователю с предложением пообщаться.
+# функционал админа в канале
+@router.chat_join_request()  # принимает заявку на вступление в закрытый канал и пишет сообщение клиенту в ЛС
 async def chat_join_request(update):
-    await update.approve()
+    await update.approve() # одобрение заявки
     text = f"Моё уважение, {update.from_user.full_name}!\n" \
            "Рад, что присоединились к моему каналу.\n" \
            "Буду рад, если Вы останетесь с нами надолго.\n" \
            "Не забудьте прочитать <a href='https://t.me/c/1655748830/4'>закрепленное сообщение</a>.\n\n" \
-           "А еще помимо канала я могу ответить на твои вопросы лично. Напиши свой вопрос и я дам на него ответ."
+           "А еще помимо канала Я могу ответить на Ваши вопросы лично. Напишите вопрос о психологии, и Я дам на него " \
+           "ответ."
     await bot.send_message(chat_id=update.from_user.id, text=text)
-
-
-@router.message()  # принимает все вводимые сообщения от пользователя и выдает ответ GPT бота с определенными
-# параметрами
-async def GPT_answer(message):
-    await bot.send_message(chat_id=message.chat.id, text='Думаю...\nСкоро напишу.')
-    answer = await GPT_request(message)
-    await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id + 1)
-    await bot.send_message(chat_id=message.chat.id, text=answer)
 
 
 async def main():
